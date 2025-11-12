d97b2458df8978c4131cfd88252140b1a500aa47
Jordan-Deitsch <jordan.b.deitsch@gmail.com>
2025-11-11 22:49:07 -0700

    Initial commit

---
+++ Shift_Register_Reader.ino
@@ -0,0 +1,87 @@
+#define PIN_LED_OUT         (13)
+#define PIN_CLK_OUT         (2)
+#define PIN_CLK_INH_OUT     (3)
+#define PIN_SERIAL_IN       (4)
+#define PIN_SHIFT_LOAD_OUT  (5)
+
+#define LOAD_TIME_MSEC    (1)     // Delay to load data into shift register
+#define NUM_BITS          (8)     // 8-bit shift register
+#define READ_FREQ_HZ      (100)   // 100 Hz = 10 msec clk period
+#define HALL_SENSOR_MASK  (0x1)   // Bit 0 of [7:0] shift register read
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(PIN_LED_OUT, OUTPUT);
+  pinMode(PIN_CLK_OUT, OUTPUT);
+  pinMode(PIN_CLK_INH_OUT, OUTPUT);
+  pinMode(PIN_SHIFT_LOAD_OUT, OUTPUT);
+  pinMode(PIN_SERIAL_IN, INPUT);
+
+  // Initialize shift register control signals
+  digitalWrite(PIN_CLK_OUT, LOW);
+  digitalWrite(PIN_CLK_INH_OUT, HIGH);
+  digitalWrite(PIN_SHIFT_LOAD_OUT, HIGH);
+}
+
+void blink_output(uint8_t pin, unsigned long delay_msec) {
+  digitalWrite(pin, HIGH);
+  delay(delay_msec);
+  digitalWrite(pin, LOW);
+  delay(delay_msec);
+}
+
+uint8_t read_shift_register() {
+  float clock_period_msec = 1000.0f / READ_FREQ_HZ;
+  uint16_t clock_half_period_msec = (uint16_t)(clock_period_msec/2.0f);
+  
+  // Check minimum clock frequency and set to 1 msec
+  if(clock_half_period_msec == 0) {
+    clock_half_period_msec = 1;
+  }
+
+  // Load data from parallel inputs into shift register
+  digitalWrite(PIN_SHIFT_LOAD_OUT, LOW);
+  delay(LOAD_TIME_MSEC);
+  digitalWrite(PIN_SHIFT_LOAD_OUT, HIGH);
+
+  // Set CLK_INH to LOW to allow data shift
+  digitalWrite(PIN_CLK_INH_OUT, LOW);
+  delay(LOAD_TIME_MSEC);
+
+  // Read Shift Register
+  uint8_t ret_value = 0;
+  for(uint8_t i=0; i<NUM_BITS; i++) {
+    ret_value = (ret_value << 1) | digitalRead(PIN_SERIAL_IN);
+    delay(clock_half_period_msec); 
+    digitalWrite(PIN_CLK_OUT, HIGH);
+    delay(clock_half_period_msec);
+    digitalWrite(PIN_CLK_OUT, LOW);
+  }
+
+  // Reset CLK_INH to HIGH after data shift complete
+  digitalWrite(PIN_CLK_INH_OUT, HIGH);
+
+  return ret_value;
+}
+
+void print_shift_register(uint8_t reg_value) {
+  // Print results of shift register
+  Serial.print("Shift Reg: 8'b");
+  for (int i=(NUM_BITS-1); i >= 0; i--) {        // Loop over each bit from MSB to LSB
+    Serial.print((reg_value >> i) & 0x1);  // Extract and print each bit
+  }
+
+  // Check if magnet is present
+  uint8_t mag_present = !(reg_value & HALL_SENSOR_MASK);
+  mag_present != 0 ? Serial.print(" : MAGNET") : Serial.print(" : NONE");
+  Serial.println();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  uint8_t shift_reg_value = read_shift_register();
+  print_shift_register(shift_reg_value);
+
+  delay(100);
+}
